fde5bd2c9d935bdc610430790c2a4366ab1e6043
Isaque <isaquesouzabraga@gmail.com>
2025-11-09 18:30:15 -0300

    style(toast): adiciona margem no toast

--- frontend/src/views/ToastWrapper.vue
+++ frontend/src/views/ToastWrapper.vue
@@ -50,6 +50,7 @@ const iconError = `<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15
     cursor: pointer;
     width: 300px;
     height: 50px;
+    margin-top: 10px;
 }
 
 .toast-icon {
